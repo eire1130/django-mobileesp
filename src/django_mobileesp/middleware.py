b2ee0b22ac3dacde7083c8dd0eea981bdddb711e
James G. Reynolds <eire1130@gmail.com>
2017-06-08 14:55:03 -0400

    Using MiddlewareMixin for django11 dep warnings, hopefully should be backwards compatitable

--- src/django_mobileesp/middleware.py
+++ src/django_mobileesp/middleware.py
@@ -1,5 +1,10 @@
 from django.conf import settings
 from django.utils.functional import SimpleLazyObject
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except:
+    class MiddlewareMixin(object):
+        pass
 
 DETECT_USER_AGENTS = getattr(settings, 'DETECT_USER_AGENTS', {})
 
@@ -8,10 +13,13 @@ def lazy_detection(request, key):
     return SimpleLazyObject( lambda: detector(request) )
 
 
-class UserAgentDetectionMiddleware(object):
+class UserAgentDetectionMiddleware(MiddlewareMixin):
     """
     Middleware to detect request's user agent
     """
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
     def process_request(self, request):
         for each in DETECT_USER_AGENTS:
             setattr( request, each, lazy_detection(request, each) )
